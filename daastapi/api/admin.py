e9469c3026ac61d483a97594f57661cfe938eb64
Domingos <domingos@dotproduct.com.br>
2023-07-25 12:00:52 -0300

    Initial models created.

--- daastapi/api/admin.py
+++ daastapi/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Document, DocumentLinkMode, DocumentRevision, VoyageDocument, EnslaverDocument, EnslavedDocument
 
-# Register your models here.
+admin.site.register(Document)
+admin.site.register(DocumentLinkMode)
+admin.site.register(DocumentRevision)
+admin.site.register(VoyageDocument)
+admin.site.register(EnslaverDocument)
+admin.site.register(EnslavedDocument)
--- daastapi/api/models.py
+++ daastapi/api/models.py
@@ -1,3 +1,65 @@
 from django.db import models
 
-# Create your models here.
+class Document(models.Model):
+    """
+    A document indexed by this API.
+    """
+    label = models.CharField(max_length=255, null=False, db_index=True)
+    originalURI = models.CharField(max_length=1024, null=False)
+    revisionNo = models.IntegerField(null=True)
+
+class DocumentRevision(models.Model):
+    """
+    A specific revision of a Document.
+    """
+    class Status(models.IntegerChoices):
+        """
+        The publication status of the document revision.
+        """
+        DRAFT = 0
+        CONTRIBUTION = 15
+        REJECTED = 99
+        PUBLISHED = 200
+
+    document = models.ForeignKey(Document, null=False, on_delete=models.CASCADE)
+    status = models.IntegerField(choices=Status.choices, db_index=True)
+    revisionNo = models.IntegerField(null=True)
+    timestamp = models.DateField(db_index=True)
+    content = models.JSONField(null=False)
+    digest = models.CharField(max_length=128)
+
+class DocumentLinkMode(models.Model):
+    """
+    A custom enumeration that describes how documents can be linked to
+    Voyages/people in the Atlantic Slave Trade.
+    """
+    label = models.CharField(max_length=64, null=False)
+
+class EntityDocument(models.Model):
+    """
+    Represents a connection between an Entity (abstract) and a Document.
+    """
+    link_mode = models.ForeignKey(DocumentLinkMode, null=False, on_delete=models.RESTRICT)
+    document = models.ForeignKey(Document, null=False, on_delete=models.RESTRICT)
+    notes = models.CharField(max_length=255, null=True)
+
+    class Meta:
+        abstract = True
+
+class EnslavedDocument(EntityDocument):
+    """
+    Connects enslaved and documents.
+    """
+    enslaved_id = models.IntegerField(null=False, db_index=True)
+
+class EnslaverDocument(EntityDocument):
+    """
+    Connects enslavers and documents.
+    """
+    enslaver_id = models.IntegerField(null=False, db_index=True)
+
+class VoyageDocument(EntityDocument):
+    """
+    Connects voyages and documents.
+    """
+    voyage_id = models.IntegerField(null=False, db_index=True)
